8429042f9f18f35721dfeb77e9290f478b8297a1
godmonera <borwonpak_du@cmu.ac.th>
2023-08-09 18:39:04 +0700

    Done

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Borwonpak Duangjun"
+            studentId="650610779"
           />
         </div>
       </body>
